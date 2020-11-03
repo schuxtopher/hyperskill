155a41a57a62cc62adce692956edde29e824b596
voidwalkr <the.schulte.christopher@gmail.com>
2020-11-04 00:37:21 +0100

    stage 1 completed

--- Multilingual Online Translator/Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/Multilingual Online Translator/task/translator/translator.py
@@ -1,1 +1,6 @@
-print('Hello, World!')
+print('Type "en" if you want to translate from French into English,'
+      ' or "fr" if you want to translate from English into French:')
+lang = input()
+print('Type the word you want to translate:')
+word = input()
+print(f'You chose "{lang}" as the language to translate "{word}" to.')
