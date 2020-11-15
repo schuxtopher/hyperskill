ca7681209af58f48e0f5d531dcfe39a2a9182ba8
voidwalkr <the.schulte.christopher@gmail.com>
2020-11-15 20:33:45 +0100

    Stage: Stage 3 completed

--- Multilingual Online Translator/Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/Multilingual Online Translator/task/translator/translator.py
@@ -4,8 +4,10 @@ from bs4 import BeautifulSoup
 
 
 def get_translation(word, lang):
+    translation_direction = {'en': 'french-english',
+                             'fr': 'english-french'}
     base_url = 'https://context.reverso.net/translation'
-    lang_url = lang_to_url(lang)
+    lang_url = '/' + translation_direction[lang]
     word_url = '/' + word
     url = base_url + lang_url + word_url
     headers = {'user-agent': 'my-app/0.0.1'}
@@ -13,22 +15,31 @@ def get_translation(word, lang):
     r = requests.get(url, headers=headers)
 
     if r.status_code:
-        print(r.status_code, 'OK')
+        print(r.status_code, 'OK', '\n')
 
-    soup = BeautifulSoup(r.content, 'html.parser')
-    translation = soup.find(id='translations-content')
-    examples = soup.find(id='examples-content')
+        soup = BeautifulSoup(r.content, 'html.parser')
+        translation_content = soup.find(id="translations-content")
+        examples_content = soup.find(id="examples-content")
 
-    print("Translations")
-    print([word for word in translation.text.split()])
-    print([word for word in examples.text.split()])
+        translations = translation_content.text.split()
 
+        examples = examples_content.text.split('\n')
+        # remove empty elements and strip unnecessary spaces from example sentences
+        examples = list(map(str.strip, filter(None, examples)))
 
-def lang_to_url(lang):
-    if lang == 'en':
-        return '/french-english'
-    elif lang == 'fr':
-        return '/english-french'
+        lang_names = {'en': 'English',
+                      'fr': 'French'}
+
+        print("Context examples:", '\n')
+
+        print(lang_names[lang] + " Translations:")
+        print('\n'.join(translations[:5]), '\n')
+
+        print(lang_names[lang] + " Examples:")
+        for i, sentence in enumerate(examples[:10]):
+            print(sentence)
+            if i % 2:
+                print()
 
 
 def start():
