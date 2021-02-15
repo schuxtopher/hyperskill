8a2922d5679d4349bae06f38f47491fc53e79075
voidwalkr <the.schulte.christopher@gmail.com>
2021-02-15 16:25:51 +0100

    Project: Stage 2, customer menu, completed - Custom template tag

--- Hypercar Service Center/Hypercar Service Center/task/hypercar/tickets/views.py
+++ Hypercar Service Center/Hypercar Service Center/task/hypercar/tickets/views.py
@@ -1,7 +1,22 @@
 from django.views import View
+from django.views.generic.base import TemplateView
 from django.http.response import HttpResponse
+
+
+services = ['Change oil',
+            'Inflate tires',
+            'Diagnostic']
 
 
 class WelcomeView(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse('<h2>Welcome to the Hypercar Service!</h2>')
+
+
+class MenuView(TemplateView):
+    template_name = 'menuPage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['services'] = services
+        return context
--- Hypercar Service Center/Hypercar Service Center/task/tests.py
+++ Hypercar Service Center/Hypercar Service Center/task/tests.py
@@ -1,3 +1,4 @@
+import re
 from urllib.error import URLError
 from urllib.request import urlopen
 from hstest.check_result import CheckResult
@@ -5,25 +6,32 @@ from hstest.test_case import TestCase
 from hstest.django_test import DjangoTest
 
 
-class HypercarWelcomeToServiceTest(DjangoTest):
+class HypercarClientMenuTest(DjangoTest):
+    ELEMENT_PATTERN = '''<a[^>]+href=['"](?P<href>[a-zA-Z/_]+)['"][^>]*>'''
 
-    def get_welcome_page(self) -> CheckResult:
+    def get_client_menu_page(self) -> CheckResult:
         try:
-            main_page = self.read_page(f'http://localhost:{self.port}/welcome')
-            if 'Welcome to the Hypercar Service!' in main_page:
-                return CheckResult.true()
-            return CheckResult.false(
-                'Main page should contain "Welcome to the Hypercar Service!" line'
-            )
+            page = self.read_page(f'http://localhost:{self.port}/menu')
+            links = re.findall(self.ELEMENT_PATTERN, page)
+            for link in (
+                '/get_ticket/change_oil',
+                '/get_ticket/inflate_tires',
+                '/get_ticket/diagnostic',
+            ):
+                if link not in links:
+                    return CheckResult.false(
+                        f'Menu page should contain <a> element with href {link}'
+                    )
+            return CheckResult.true()
         except URLError:
             return CheckResult.false(
-                'Cannot connect to the /welcome page.'
+                'Cannot connect to the /menu page.'
             )
 
     def generate(self):
         return [
             TestCase(attach=self.check_server),
-            TestCase(attach=self.get_welcome_page),
+            TestCase(attach=self.get_client_menu_page),
         ]
 
     def check(self, reply, attach):
@@ -31,4 +39,4 @@ class HypercarWelcomeToServiceTest(DjangoTest):
 
 
 if __name__ == '__main__':
-    HypercarWelcomeToServiceTest('hypercar.manage').run_tests()
+    HypercarClientMenuTest('hypercar.manage').run_tests()
