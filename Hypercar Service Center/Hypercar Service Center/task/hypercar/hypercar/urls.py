617c55cffb82b49e91999112279d6f05c138dd07
voidwalkr <the.schulte.christopher@gmail.com>
2021-02-16 19:23:46 +0100

    Project: Stage 5, completed

--- Hypercar Service Center/Hypercar Service Center/task/hypercar/hypercar/urls.py
+++ Hypercar Service Center/Hypercar Service Center/task/hypercar/hypercar/urls.py
@@ -14,11 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, re_path
-from tickets.views import WelcomeView, MenuView, TicketView
+from tickets.views import (WelcomeView, MenuView, TicketView, ProcessingView,
+                           NextView)
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path('welcome/', WelcomeView.as_view()),
     path('menu/', MenuView.as_view()),
-    re_path('get_ticket\/(?P<service>(change_oil|inflate_tires|diagnostic))', TicketView.as_view())
+    re_path('get_ticket\/(?P<service>(change_oil|inflate_tires|diagnostic))',
+            TicketView.as_view()),
+    path('processing', ProcessingView.as_view()),
+    path('processing/', RedirectView.as_view(url='/processing')),
+    path('next/', NextView.as_view()),
+    path('', RedirectView.as_view(url='/next')),
 ]
--- Hypercar Service Center/Hypercar Service Center/task/hypercar/tickets/views.py
+++ Hypercar Service Center/Hypercar Service Center/task/hypercar/tickets/views.py
@@ -1,14 +1,16 @@
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.http.response import HttpResponse
-
+from django.shortcuts import redirect
+from collections import deque
 
 services = {'change_oil': 2,
             'inflate_tires': 5,
             'diagnostic': 30}
-line_of_cars = {'change_oil': [],
-                'inflate_tires': [],
-                'diagnostic': []}
+line_of_cars = {'change_oil': deque([]),
+                'inflate_tires': deque([]),
+                'diagnostic': deque([])}
+next_ticket = None
 
 
 class WelcomeView(View):
@@ -17,7 +19,7 @@ class WelcomeView(View):
 
 
 class MenuView(TemplateView):
-    template_name = 'menuPage.html'
+    template_name = 'menu.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -40,18 +42,53 @@ class TicketView(TemplateView):
         context['service'] = service
         context['ticket_number'] = self.ticket_number
         context['minutes_to_wait'] = self.get_minutes_to_wait(service)
-        line_of_cars[service].append(self.ticket_number)
+        line_of_cars[service].appendleft(self.ticket_number)
         return context
 
     def get_minutes_to_wait(self, service):
         if service == 'change_oil':
-            minutes_to_wait = services[service] * (len(line_of_cars['change_oil']))
+            minutes_to_wait = (services[service]
+                               * (len(line_of_cars['change_oil'])))
             return minutes_to_wait
         elif service == 'inflate_tires':
-            minutes_to_wait = services[service] * (len(line_of_cars['inflate_tires']))
-            queue = self.get_minutes_to_wait('change_oil')
-            return minutes_to_wait + queue
+            minutes_to_wait = (services[service]
+                               * (len(line_of_cars['inflate_tires']))
+                               + self.get_minutes_to_wait('change_oil'))
+            return minutes_to_wait
         elif service == 'diagnostic':
-            minutes_to_wait = services[service] * (len(line_of_cars['diagnostic']))
-            queue = self.get_minutes_to_wait('inflate_tires')
-            return minutes_to_wait + queue
+            minutes_to_wait = (services[service]
+                               * (len(line_of_cars['diagnostic']))
+                               + self.get_minutes_to_wait('inflate_tires'))
+            return minutes_to_wait
+
+
+class ProcessingView(TemplateView):
+    template_name = 'processing.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['queue'] = line_of_cars
+        return context
+
+    def post(self, request, *args, **kwargs):
+        global next_ticket
+        next_ticket = self.pop_next_ticket()
+        return redirect('/processing')
+
+    @staticmethod
+    def pop_next_ticket():
+        if line_of_cars['change_oil']:
+            return line_of_cars['change_oil'].pop()
+        elif line_of_cars['inflate_tires']:
+            return line_of_cars['inflate_tires'].pop()
+        elif line_of_cars['diagnostic']:
+            return line_of_cars['diagnostic'].pop()
+
+
+class NextView(TemplateView):
+    template_name = 'next.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['next_ticket'] = next_ticket
+        return context
